0cca1d8e20857a0369d4dbcc1ac5978568b1be53
Georgeches <georgechesire83@gmail.com>
2023-04-29 16:30:05 +0300

    Done assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,31 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [filterMethod, setMethod] = useState('')
+
+  let filteredArr;
+  if(filterMethod!==''){
+    filteredArr = items.filter(item=>item.category===filterMethod)
+  }
+
+  else{
+    filteredArr = [...items]
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={e=>setMethod(e.target.value)}>
+          <option value="">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredArr.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
